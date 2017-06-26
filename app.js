5753ad4b82763335b21cc606a6fc6357d41ad71e
vishnuvenugopal007 <vishnu.venugopal@live.com>
2017-06-26 14:41:06 -0700

    issue-2

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var app = express()
 var request = require('request')
 
 app.get('/results', function(req, res){
-  res.send('Helo, it works!')
-  request('http://omdbapi.com/?s=california&apikey=thewdb', function(error, res, req){
+
+  var query = req.query.search;
+  var url = 'http://omdbapi.com/?s=' + query + '&apikey=thewdb'
+
+  request(url, function(error, res, req){
     if(!error && response.statusCode == 200){
       var data = JSON.parse(body)
       res.render('results', {data: data})
